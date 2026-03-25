c79fe2e486e339e76f498a0cb0af0ea236b40837
ET4EREAL <mars1235799@gmail.com>
2026-03-25 22:42:15 +0500

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest  # Импортируем pytest
 from playwright.sync_api import sync_playwright, \
-    Page  # Импортируем класс страницы, будем использовать его для аннотации типов
+    Page, Playwright  # Импортируем класс страницы, будем использовать его для аннотации типов
 
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
@@ -15,3 +15,37 @@ def chromium_page() -> Page:  # Аннотируем возвращаемое ф
 
         # Закрываем браузер после выполнения тестов
         browser.close()
+
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill("username")
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill("password")
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    context.storage_state(path="browser-state.json")
+    page.close()
+    context.close()
+    browser.close()
+
+@pytest.fixture(scope="function")
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+    yield page
+    context.close()
+    browser.close()
