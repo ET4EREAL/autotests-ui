184f25c2118d717bd0980b888f7a2634dba6b87c
ET4EREAL <mars1235799@gmail.com>
2026-04-18 00:44:52 +0500

    Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- pages/courses/courses_list_page.py
+++ pages/courses/courses_list_page.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.navigation.navbar_component import NavbarComponent
 from components.navigation.sidebar_component import SidebarComponent
@@ -6,6 +6,8 @@ from components.views.empty_view_component import EmptyViewComponent
 from components.courses.course_view_component import CourseViewComponent
 from components.courses.courses_list_toolbar_view_component import CoursesListToolbarViewComponent
 from pages.base_page import BasePage
+from elements.button import Button
+from elements.text import Text
 
 
 class CoursesListPage(BasePage):
@@ -19,20 +21,24 @@ class CoursesListPage(BasePage):
         self.toolbar_view = CoursesListToolbarViewComponent(page)
 
         # Заголовок и кнопка создания курса
-        self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
+        self.courses_title = Text(page, 'courses-list-toolbar-title-text', 'Title')
+        self.create_course_button = Button(page, 'courses-list-toolbar-create-course-button', 'Create course')
 
         # Меню курса
-        self.course_menu_button = page.get_by_test_id('course-view-menu-button')
-        self.course_edit_menu_item = page.get_by_test_id('course-view-edit-menu-item')
-        self.course_delete_menu_item = page.get_by_test_id('course-view-delete-menu-item')
+        self.course_menu_button = Button(page, 'course-view-menu-button', 'Menu')
+        self.course_edit_menu_item = Button(page, 'course-view-edit-menu-item', 'Edit')
+        self.course_delete_menu_item = Button(page, 'course-view-delete-menu-item', 'Delete')
 
     def check_visible_courses_title(self):
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text('Courses')
+        self.courses_title.check_visible()
+        self.courses_title.check_have_text('Courses')
 
     def check_visible_empty_view(self):
         self.empty_view.check_visible(
             title='There is no results',
             description='Results from the load test pipeline will be displayed here'
         )
+
+    def click_edit(self):
+        self.course_menu_button.click()
+        self.course_edit_menu_item.click()
